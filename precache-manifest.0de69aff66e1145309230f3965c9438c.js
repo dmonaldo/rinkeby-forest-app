50fa02ebcf6c87bf74ffc8d35b66e5c67bf5b830
Dante Monaldo <dante.monaldo@gmail.com>
2021-10-31 15:31:15 -0700

    Updates

---
+++ precache-manifest.0de69aff66e1145309230f3965c9438c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f092540f82451e6f14584378c21e235",
+    "url": "https://rinkebyforest.dantemonaldo.com/index.html"
+  },
+  {
+    "revision": "a85538bf4db500f5bfe9",
+    "url": "https://rinkebyforest.dantemonaldo.com/static/css/main.40d4a754.chunk.css"
+  },
+  {
+    "revision": "119b40f4dca152180cfd",
+    "url": "https://rinkebyforest.dantemonaldo.com/static/js/2.58e1a247.chunk.js"
+  },
+  {
+    "revision": "a85538bf4db500f5bfe9",
+    "url": "https://rinkebyforest.dantemonaldo.com/static/js/main.1488225f.chunk.js"
+  },
+  {
+    "revision": "ce07cee493b4856d642b",
+    "url": "https://rinkebyforest.dantemonaldo.com/static/js/runtime-main.3086004c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3cea8422ee9d267d34b916a476e05896.js"
+  "https://rinkebyforest.dantemonaldo.com/precache-manifest.0de69aff66e1145309230f3965c9438c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://rinkebyforest.dantemonaldo.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
