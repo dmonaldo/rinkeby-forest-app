13c2c88c4fd831017534c508eb2026040779922f
Dante Monaldo <dante.monaldo@gmail.com>
2021-10-31 13:12:35 -0700

    include build directory

---
+++ build/precache-manifest.359453dcfef477529012a20772ce0aa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8e96ef30058c8434df68fd4328eb36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a456c8951fbc77a80704",
+    "url": "/static/css/main.5d4c0953.chunk.css"
+  },
+  {
+    "revision": "3f90e4c0c008aa3fec3a",
+    "url": "/static/js/2.8e77d7d9.chunk.js"
+  },
+  {
+    "revision": "a456c8951fbc77a80704",
+    "url": "/static/js/main.820ff0ce.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
