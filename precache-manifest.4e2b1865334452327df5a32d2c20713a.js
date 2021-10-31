f5d8dfda3d3acab1b38d2860182be4d2d3dcd366
Dante Monaldo <dante.monaldo@gmail.com>
2021-10-31 13:18:14 -0700

    Updates

---
+++ precache-manifest.4e2b1865334452327df5a32d2c20713a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8034dfeadbdd58be53f4a234ec749e1",
+    "url": "/rinkeby-forest-app/index.html"
+  },
+  {
+    "revision": "a456c8951fbc77a80704",
+    "url": "/rinkeby-forest-app/static/css/main.5d4c0953.chunk.css"
+  },
+  {
+    "revision": "3f90e4c0c008aa3fec3a",
+    "url": "/rinkeby-forest-app/static/js/2.8e77d7d9.chunk.js"
+  },
+  {
+    "revision": "a456c8951fbc77a80704",
+    "url": "/rinkeby-forest-app/static/js/main.820ff0ce.chunk.js"
+  },
+  {
+    "revision": "239dd5c7c31a735ccab0",
+    "url": "/rinkeby-forest-app/static/js/runtime-main.f54f2812.js"
+  }
+]);
